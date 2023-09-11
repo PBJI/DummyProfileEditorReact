424db96a20d02a007244fce9ab1a8f68b49e22a3
DrunkenSoul <purshotamjainpj@gmail.com>
2023-09-12 02:16:39 +0530

    Completely tested app

---
+++ precache-manifest.fdbfe84d9d57799dc42efaa770a57ab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "38cb10ec225b752f2f7d",
+    "url": "/static/css/main.bd2f95f5.chunk.css"
+  },
+  {
+    "revision": "38cb10ec225b752f2f7d",
+    "url": "/static/js/main.38cb10ec.chunk.js"
+  },
+  {
+    "revision": "975f63afe5057efb9a20",
+    "url": "/static/js/1.975f63af.chunk.js"
+  },
+  {
+    "revision": "cf438c2415ce5d7ff74e",
+    "url": "/static/js/2.cf438c24.chunk.js"
+  },
+  {
+    "revision": "b9842c9c4c236a614e5d",
+    "url": "/static/js/runtime~main.b9842c9c.js"
+  },
+  {
+    "revision": "b0786fcb671c0dcf99f1c02cb048d898",
+    "url": "/index.html"
+  }
+];
